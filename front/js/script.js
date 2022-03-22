dd7e8bc98a31647db31afceba16f43cd3740ecce
DVDFE <davidfe.pro@gmail.com>
2022-03-22 19:42:36 +0100

    addProducts

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,14 @@
 fetch("http://localhost:3000/api/products")
   .then((res) => res.json())
-  .then((data) => console.log(data))
-
-
+  .then((data) => addProducts(data))
+  
+  function addProducts(data) {
+    
+    const imageUrl= data[0].imageUrl
+    const link = document.createElement("a")
+    link.href = "http://localhost:3000/images/kanap01.jpeg"
+    link.text = "Photo d'un canapé bleu, deux places"
+    const items = document.querySelector("#items")
+    items.appendChild(link)
+  } 
+  
