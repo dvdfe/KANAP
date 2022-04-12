31cb3ae40300d994cd89abe0afeaf0eb90f30c8b
DVDFE <davidfe.pro@gmail.com>
2022-04-12 18:35:46 +0200

    product

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,10 @@
 const productSelected = window.location.search;
+const urlParams = new URLSearchParams(productSelected) 
+const id = urlParams.get("id")
+console.log(id)
 
- 
+fetch(`http://localhost:3000/api/products/${id}`)
+.then((data) => data.json())
+.then((data) => {
+    console.log(data)
+})
--- front/js/script.js
+++ front/js/script.js
@@ -1,65 +1,54 @@
 fetch("http://localhost:3000/api/products")
   .then((res) => res.json())
-  .then((data) => {
-    return addProducts(data) 
-  })
+  .then((data) => {addProducts(data)})
 
 function addProducts(data) {
-    const _id= data[0]._id
-    const imageUrl = data[0].imageUrl
-    const altTxt = data[0].altTxt
-    const name = data[0].name
-    const description = data[0].description
 
-    data.forEach((canapé) => {
-      const { _id, imageUrl, altTxt, name, description} = canapé
-      
-      const image = makeImage(imageUrl,altTxt)
-      const link = makeLink(_id)
-      const article = makeArticle() 
-      const h3 = makeH3(name)
-      const p = makeParagraph(description)
-      article.appendChild(image)
-      article.appendChild(h3)
-      article.appendChild(p)
-      appendArticleToLink(link, article)
-    });
-  } 
-  
-function makeLink(id){
-    const link = document.createElement("a")
-    link.href = "./product.html?id=" + id
-    return link
-  }
+  data.forEach((sofa) => {
+    const { _id, imageUrl, altTxt, name, description } = sofa;
+
+    const image = makeImage(imageUrl, altTxt);
+    const link = makeLink(_id);
+    const article = document.createElement("article");
+    const h3 = makeH3(name);
+    const p = makeParagraph(description);
+    article.appendChild(image);
+    article.appendChild(h3);
+    article.appendChild(p);
+    appendArticleToLink(link, article);
+  });
+}
+
+function makeLink(id) {
+  const link = document.createElement("a");
+  link.href = "./product.html?id=" + id;
+  return link;
+}
 
 function appendArticleToLink(link, article) {
-    const items = document.querySelector("#items")
-    items.appendChild(link)
-    link.appendChild(article)
-  }
-
-function makeImage(imageUrl, altTxt){
-  const image = document.createElement("img")
-  image.src = imageUrl
-  image.alt = altTxt
-  return image
+  const items = document.querySelector("#items");
+  items.appendChild(link);
+  link.appendChild(article);
 }
 
-function makeArticle(){
-  const article = document.createElement("article")
-  return article
+function makeImage(imageUrl, altTxt) {
+  const image = document.createElement("img");
+  image.src = imageUrl;
+  image.alt = altTxt;
+  return image;
 }
 
-function makeH3(name){
-  const h3 = document.createElement("h3")
-  h3.textContent = name
-  h3.classList.add("productName")
-  return h3
+
+function makeH3(name) {
+  const h3 = document.createElement("h3");
+  h3.textContent = name;
+  h3.classList.add("productName");
+  return h3;
 }
 
-function makeParagraph(description){
-  const p = document.createElement("p")
-  p.textContent = description
-  p.classList.add("productDescription")
-  return p
+function makeParagraph(description) {
+  const p = document.createElement("p");
+  p.textContent = description;
+  p.classList.add("productDescription");
+  return p;
 }
