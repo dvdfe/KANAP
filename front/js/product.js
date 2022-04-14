6d27ed3241cff8704e3475ea7acab9c56e39d5b0
DVDFE <davidfe.pro@gmail.com>
2022-04-14 13:42:40 +0200

    description

--- front/js/product.js
+++ front/js/product.js
@@ -6,5 +6,35 @@ console.log(id)
 fetch(`http://localhost:3000/api/products/${id}`)
 .then((data) => data.json())
 .then((data) => {
-    console.log(data)
+    product(data)
 })
+
+function product(sofa){
+    const {altTxt, colors, description, imageUrl, name, price, _id} = sofa
+    makeImage(imageUrl,altTxt)
+    makeTitle(name)
+    makePrice(price)
+    makeDescription(description)
+
+}
+
+function makeImage(imageUrl,altTxt){
+    const image = document.createElement('img')
+    image.src = imageUrl
+    image.altTxt = altTxt
+    const parent = document.querySelector('.item__img')
+    parent.appendChild(image)
+    return image
+}
+
+function makeTitle(name){
+    document.querySelector('#title').textContent = name  
+}
+
+function makePrice(price){
+    document.querySelector('#price').textContent = price
+}
+
+function makeDescription(description){
+    document.querySelector('#description').textContent = description
+}
